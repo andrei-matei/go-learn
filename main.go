31194e52a3da0e2309aa6f8e359c16c2289aaa97
Andrei <andrei.matei@bawagpsk.com>
2020-12-20 23:44:51 +0100

    First main

--- main.go
+++ main.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Index page")
-
-}
-
 func main() {
-
-	http.HandleFunc("/", index)
-	http.ListenAndServe(":9090", nil)
+	fmt.Println("Base service.")
 
 }
